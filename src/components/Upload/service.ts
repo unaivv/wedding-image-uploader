0c04f445d8baba876625f57106d74138de50316b
Unaivv <vidalvisounai@gmail.com>
2025-06-10 15:07:24 +0200

    feature: allow delete on remote server

--- src/components/Upload/service.ts
+++ src/components/Upload/service.ts
@@ -1,6 +1,10 @@
 export const deleteFile = async (fileId: string): Promise<boolean> => {
     try {
-        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/files/delete?fileId=${fileId}`);
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/files/delete?fileId=${fileId}`, {
+            headers: {
+                "ngrok-skip-browser-warning": "69420", //TODO: ONLY FOR DEV
+            }
+        });
 
         if (!response.ok) {
             console.error('Failed to delete file:', response);
