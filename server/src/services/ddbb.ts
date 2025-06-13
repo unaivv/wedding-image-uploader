a7ea2ecfaffb82aa05d9306ea9dc3c03caea632e
Unaivv <vidalvisounai@gmail.com>
2025-06-13 20:17:38 +0200

    feat: update MongoDB connection to use environment variable for URI

--- server/src/services/ddbb.ts
+++ server/src/services/ddbb.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connect = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/wedding-uploader');
+        await mongoose.connect(process.env.MONGODB_URI || '');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
         throw error;
