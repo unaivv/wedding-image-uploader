32b8640e70ea61c710dbae8873d9dd60ff1f360e
Unai <unai.v@hastee.com>
2026-04-30 22:10:42 +0200

    css variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'rsuite/dist/rsuite-no-reset.min.css';
+import './rsuite-theme.css';
 import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(<App />
