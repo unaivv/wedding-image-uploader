27905c5c60cddcb46925e149adddf6dc84f97599
Unai <unai.v@hastee.com>
2026-04-30 15:44:01 +0200

    debug: log winnerFile.id

--- src/components/Challenges/Challenge/index.tsx
+++ src/components/Challenges/Challenge/index.tsx
@@ -32,11 +32,10 @@ const Challenge = ({ challenge }: IChallengeProps) => {
     // winnerPhoto needs to be defined before handleOpenWinnerLightbox
     const winnerParticipant = challenge?.winner ? challenge.participants.find(p => p.user._id === challenge.winner!._id) : null;
     const winnerFile = winnerParticipant?.file;
-    console.log('[Challenge] winnerFile:', winnerFile, 'winnerFile?.id:', winnerFile?.id);
+    console.log('[Challenge] winnerFile:', winnerFile, 'winnerFile?.id:', winnerFile?.id, 'winnerFile?._id:', winnerFile?._id);
     const winnerPhoto: IPhoto | null = winnerFile
         ? {
               id: winnerFile.id || winnerFile._id || 'MISSING_ID',
-              src: winnerFile.compressedSrc,
               src: winnerFile.compressedSrc,
               fullSrc: winnerFile.fullSrc ?? winnerFile.compressedSrc,
               alt: winnerFile.id,
